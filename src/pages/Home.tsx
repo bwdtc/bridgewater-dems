dc99a3b0045d15acfde4526f751eb2b0d4c95402
bwdtc <info@bridgewaterdems.org>
2025-11-09 13:21:32 -0500

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,7 +14,7 @@ const Home = () => {
           <div className="lg:col-span-2">
             <div className="bg-white p-8 rounded-lg shadow-sm">
               <p className="text-lg text-gray-700 leading-relaxed mb-4">
-                The Bridgewater Democratic Town Committee is the voice and face of the Democratic Party in Bridgewater, Connecticut. We love Bridgewater and think is a great place to live. We are grateful for all that nature has given us here.
+                The Bridgewater Democratic Town Committee is the voice and face of the Democratic Party in Bridgewater, Connecticut. We love Bridgewater and think it is a great place to live. We are grateful for all that nature has given us here.
               </p>
               <p className="text-lg text-gray-700 leading-relaxed mb-4">
                 We understand that it takes effort, discipline and skill to care for our beautiful surroundings, for the town's infrastructure and its operations, and for all of the residents who have chosen to make Bridgewater their home.
